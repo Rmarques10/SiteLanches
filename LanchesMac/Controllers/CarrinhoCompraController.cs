6bb21e3f0bee6cd50fa8d43d236253a48f998701
Rafael Marques <rafaelmarques.rm86@gmail.com>
2023-03-23 22:10:52 -0300

    Implementando a autorização - Authorize

--- LanchesMac/Controllers/CarrinhoCompraController.cs
+++ LanchesMac/Controllers/CarrinhoCompraController.cs
@@ -1,6 +1,7 @@
 ﻿using LanchesMac.Models;
 using LanchesMac.Repositories.Interfaces;
 using LanchesMac.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesMac.Controllers
@@ -29,6 +30,7 @@ namespace LanchesMac.Controllers
             return View(carrinhoCompraVm);
         }
 
+        [Authorize]
         public RedirectToActionResult AdicionarItemNoCarrinhoCompra(int lancheId)
         {
             var lancheSelecionado = _lancheRepository.Lanches
@@ -40,6 +42,7 @@ namespace LanchesMac.Controllers
             return RedirectToAction("Index");
         }
 
+        [Authorize]
         public IActionResult RemoverItemDoCarrinhoCompra(int lancheId)
         {
             var lancheSelecionado = _lancheRepository.Lanches
