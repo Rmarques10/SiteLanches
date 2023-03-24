2a3debe9de7ada8a55b1aa8b46935f0127f80f08
Rafael Marques <rafaelmarques.rm86@gmail.com>
2023-03-23 22:44:17 -0300

    Area Admin - Configuração do AdminController

--- LanchesMac/Startup.cs
+++ LanchesMac/Startup.cs
@@ -63,6 +63,10 @@ public class Startup
         app.UseEndpoints(endpoints =>
         {
             endpoints.MapControllerRoute(
+             name: "areas",
+             pattern: "{area:exists}/{controller=Admin}/{action=Index}/{id?}");
+
+            endpoints.MapControllerRoute(
                 name: "categoriaFiltro",
                 pattern: "Lanche/{action}/{categoria?}",
                 defaults: new { Controller = "Lanche", AccessViolationException = "List" });
